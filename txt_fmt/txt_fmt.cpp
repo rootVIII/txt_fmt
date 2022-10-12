fe649024be723895e9a60bb176a331829b061310
rootVIII <colleyloyejames@gmail.com>
2022-10-12 18:37:01 -0400

    add timestamp to output file

--- txt_fmt/txt_fmt.cpp
+++ txt_fmt/txt_fmt.cpp
@@ -1,14 +1,31 @@
 ﻿#include "txt_fmt.h"
+#include <ctime>
+#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <regex>
 #include <sstream>
+#include <windows.h>
 
 
 TxtFmt::TxtFmt(std::string path, const int max_chars) : path_(std::move(path)), max_chars_(max_chars) 
 {
-    // TODO add datestamp here
-    output_file_path = path_ + ".out";
+    const auto start = std::chrono::system_clock::now();
+    const auto legacy_start = std::chrono::system_clock::to_time_t(start);
+    char tm_buff[30];
+    if (ctime_s(tm_buff, sizeof(tm_buff), &legacy_start) != 0)
+        throw std::runtime_error("system error occurred while retrieving local time");
+    std::string datestamp;
+    const auto stamp = std::string(tm_buff);
+    for (auto &letter : stamp)
+    {
+        if (letter == 0x20)
+            datestamp += '_';
+        else if (letter != 0x0A && letter != 0x3A)
+            datestamp += letter;
+    }
+
+    output_file_path = path_ + "." + datestamp;
 }
 
 std::string TxtFmt::get_output_file_path() const
@@ -36,7 +53,6 @@ void TxtFmt::process_text(const std::string &file_data) const
     if (!(file_handle.is_open()))
         throw std::runtime_error("failed to open file for write operation");
     
-
     std::vector<std::string> text_blocks;
     const std::regex new_lines(R"(\n)");
     std::copy(std::sregex_token_iterator(file_data.begin(), file_data.end(), new_lines, -1),
